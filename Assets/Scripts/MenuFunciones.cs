06fdd1a056e11b6ed93e049bb2cf4ab50dfaa4e4
TeoBaiocchi <teobaiocchiensinck@gmail.com>
2021-09-12 22:03:24 -0300

    Removido el, entre comillas, nivel. Pero agregado un menu con 1 (un) botón funcional, que te lleva al TechTest

---
+++ Assets/Scripts/MenuFunciones.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MenuFunciones : MonoBehaviour
+{
+    public void cargarEscenaPrueba(){
+        SceneManager.LoadScene("TechTest");
+    }
+}
---
+++ Assets/Scripts/NoDestruir.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoDestruir : MonoBehaviour
+{
+    public static NoDestruir instance; //la idea de esto es hacer que haya
+    // una UNICA instancia de este objeto, y no destruirla nunca cambiando de escena
+    // pero destruyendo todos sus duplicados
+    private void Start() {
+        if(instance != null){
+            Destroy(this.gameObject);
+            return;
+        }
+        instance = this;
+        GameObject.DontDestroyOnLoad(this.gameObject);
+    }
+    
+}
