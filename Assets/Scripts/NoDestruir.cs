0ede05c6c901f28e0230e78a2d30a4b7dc8087db
TeoBaiocchi <teobaiocchiensinck@gmail.com>
2021-09-13 00:20:29 -0300

    Corregidos algunos defectos básicos del menú

--- Assets/Scripts/NoDestruir.cs
+++ Assets/Scripts/NoDestruir.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class NoDestruir : MonoBehaviour
 {
-    public static NoDestruir instance; //la idea de esto es hacer que haya
+    public static NoDestruir Instance; //la idea de esto es hacer que haya
     // una UNICA instancia de este objeto, y no destruirla nunca cambiando de escena
     // pero destruyendo todos sus duplicados
     private void Start() {
-        if(instance != null){
+        if(Instance != null){
             Destroy(this.gameObject);
             return;
         }
-        instance = this;
+        Instance = this;
         GameObject.DontDestroyOnLoad(this.gameObject);
     }
     
