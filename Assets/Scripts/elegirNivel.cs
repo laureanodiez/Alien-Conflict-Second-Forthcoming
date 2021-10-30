3b9d4b331629150d913a04d3ff81797dddee554d
Tomás Maiza <tomimaiza@gmail.com>
2021-10-30 13:07:56 -0300

    Elegir nivel desde el nexo

---
+++ Assets/Scripts/elegirNivel.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class elegirNivel : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject pantalla;
+    public GameObject menu;
+    // public GameObject nexo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector3.Distance(transform.position, player.transform.position) < 3.0f) {
+            pantalla.SetActive(true);
+            if(Input.GetKeyDown(KeyCode.E)) {
+                menu.SetActive(true);
+                // nexo.SetActive(false);
+                
+            }
+        }
+        else {
+            pantalla.SetActive(false);
+        }
+    }
+
+    public void cargarNivel(string nivel) {
+        SceneManager.LoadScene(nivel);
+    }
+
+    public void volverAlNexo() {
+        menu.SetActive(false);
+    }
+}
+
