15997f701acab103f05f2b1b2a2585c06dde9320
Tomás Maiza <tomimaiza@gmail.com>
2021-11-10 23:37:36 -0300

    Nivel 2

---
+++ Assets/Scripts/cursorLlave.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cursorLlave : MonoBehaviour
+{
+    public GameObject puerta;
+    public GameObject llave;
+    
+
+    void OnCollisionEnter(Collision collision) {
+        Destroy(llave);
+        Destroy(puerta);
+    }
+
+
+}
